4f79631ff18e29e03b48cab20161cd5f91f23e89
Suchit Sharma <suchitsharma@MacBook-Pro.local>
2020-06-02 23:52:20 -0600

    created menu page and rerouted

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 from flask import Flask, request, render_template
+from sudopy import Sudoku
 from sudopy_kings import Sudoku_Kings
 
 app = Flask(__name__)
 
 sudoku_grid_list = []
 @app.route('/')
-@app.route('/index')
-def index():
-    sudoku_grid_list = []
-    return render_template('index.html')
+def menu():
+    return render_template('menu.html')
 
-@app.route("/solution", methods=['POST', 'GET'])
+@app.route('/input_classic')
+def input_classic():
+    sudoku_grid_list = []
+    return render_template('input_classic.html')
+
+@app.route('/input_kings')
+def input_kings():
+    sudoku_grid_list = []
+    return render_template('input_kings.html')
+
+@app.route('/solution', methods=['POST', 'GET'])
 def solution():
     sudoku_grid_list = []
     if request.method == 'POST':
@@ -25,13 +34,15 @@ def solution():
                     row_list.append(int(content))
             sudoku_grid_list.append(row_list)
 
-        s = Sudoku_Kings(sudoku_grid_list)
-        print(s)
+        if "/input_classic" in request.referrer:
+            s = Sudoku(sudoku_grid_list)
+        elif "/input_kings" in request.referrer:
+            s = Sudoku_Kings(sudoku_grid_list)
         s.solve()
-        print(s)
+
         return render_template('solution.html', solved_array = s.return_array())
     else:
-        return render_template('index.html')
+        return render_template('menu.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
